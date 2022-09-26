6b2db0b0d3c709b9736864043da24c7bed1b0e25
kekeli92870 <kekeli92870@gmail.com>
2022-09-26 14:50:56 +0800

    bs4爬ptt存成txt

--- crawler_ptt.py
+++ crawler_ptt.py
@@ -3,6 +3,8 @@
 
 使用 Python 的 Requests 和 Beautiful Soup 函式庫，實作一個網路爬蟲
 ，利用傳送 cookie 的方式，突破未滿十八歲的按鈕檢查限制，取得 PTT 八卦版文章的標題
+
+使用 txt 儲存
 '''
 # # 引入套件
 # import requests
@@ -22,14 +24,31 @@ url = 'https://www.ptt.cc/'
 web = requests.get('https://www.ptt.cc/bbs/Gossiping/index.html', cookies={'over18':'1'})
 soup = BeautifulSoup(web.text, "html.parser")
 # 取得 class 為 title 的 div 內容
-titles = soup.find_all('div', class_='title')     
+titles = soup.find_all('div', class_='title')   
+# 建立 output 變數 
+output = ''            
+
 for i in titles:
     # 判斷如果不為 None
     if i.find('a') != None:           
-        # 取得 div 裡 a 的內容，使用 get_text() 取得文字              
-        print(i.find('a').get_text())              
-        # 使用 ['href'] 取得 href 的屬性   
-        print(url + i.find('a')['href'], end='\n\n')  
+        # # 取得 div 裡 a 的內容，使用 get_text() 取得文字              
+        # print(i.find('a').get_text())              
+        # # 使用 ['href'] 取得 href 的屬性   
+        # print(url + i.find('a')['href'], end='\n\n') 
+
+        # 將資料一次記錄到 output 變數裡
+        output = output + i.find('a').get_text() + '\n' + url + i.find('a')['href'] + '\n\n'
+
+print(output)
+
+# 建立並開啟純文字文件
+with open('ptt-Gossiping.txt', 'w', encoding='UTF-8') as f:
+    # 將資料寫入檔案裡
+    f.write(output) 
+
+# f = open('ptt-Gossiping.txt', 'w', encoding='UTF-8')   # 建立並開啟純文字文件
+# f.write(output)     # 將資料寫入檔案裡
+# f.close()
 
 
 
